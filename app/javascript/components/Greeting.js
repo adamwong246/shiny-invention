005e30e83ef26a994f1622f2c8bad39086585c36
adam wong <adamwong246@gmail.com>
2022-11-09 16:49:50 -0800

    react added to codebase

---
+++ app/javascript/components/Greeting.js
@@ -0,0 +1,14 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class Greeting extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <p>hello react</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Greeting
